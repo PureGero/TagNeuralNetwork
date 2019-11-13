247bfb4ee9158c689f90ac0e425a93f5e0fb3541
PureGero <puregero@gmail.com>
2019-11-13 14:16:33 +1000

    Train against previous agents alongside the current agents

--- trainer.js
+++ trainer.js
@@ -6,13 +6,15 @@ const fs = require('fs');
 const NeuralNetwork = require('./neuralnetwork.js');
 const Neat = require('./neat.js');
 
+var CPU_TARGET = 10;
+
 var speed = 0.5;
     
 var dots = [];
 
 var GAMES = 256;
 var GAMES_PER_GENERATION = 20;
-var GENERATIONS_PER_TRAINING = 20;
+var GENERATIONS_PER_TRAINING = 25;
 var SECONDS_PER_GAME = 5;
 var PLAYER_COUNT = 2;
 var TAGGER_COUNT = 1;
@@ -22,13 +24,16 @@ var generations = 0;
 var games = [];
 var dt = 1/20;
 var training = 0;
+var moves = 0;
 
 var writtenPlayers = [];
+var previousBest = [];
 var players = [];
 var baseNetwork = NeuralNetwork.createHiddenLayeredNetwork(PLAYER_COUNT*2, 4, 4);
 for (var i = 0; i < PLAYER_COUNT; i++) {
     players.push(new Neat(GAMES, 4, baseNetwork));
     players[i].evolveRate = 0.9;
+    previousBest.push(baseNetwork);
 }
 
 app.get('/', function(req, res){
@@ -40,58 +45,9 @@ io.on('connection', function(socket) {
     socket.emit('msg', (training?'Red':'Blue') + ' dot has been trained');
 });
 
-function newGeneration() {
-    if (generations % GAMES_PER_GENERATION == 0) {
-        //console.log("New Generation", Math.floor(generations/GAMES_PER_GENERATION) ,"of", training);
-        players[training].evolve();
-    }
-    if (generations % (GENERATIONS_PER_TRAINING*GAMES_PER_GENERATION) == 0) {
-        training = (training + 1) % PLAYER_COUNT;
-        console.log("Begin training of", training);
-        io.emit('msg', (training?'Red':'Blue') + ' dot has been trained');
-        writeBestToDisk();
-    }
-    ticks = 0;
-    games = [];
-    for (var j = 0; j < GAMES; j++) {
-        dots = [];
-        for (var i = 0; i < players.length; i++) {
-            var x = Math.random();
-            var y = Math.random();
-            dots.push({
-                x: x, 
-                y: y, 
-                tagged: false, 
-                tagger: i < TAGGER_COUNT
-            });
-        }
-        games.push({dots: dots, moves: 0, toMove: true});
-    }
-    run();
-}
-
-function finishGeneration() {
-    for (var j = 0; j < GAMES; j++) {
-        dots = games[j].dots;
-        var dot = dots[training];
-        if (!dot.tagger && !dot.tagged)
-            players[training].scores[j] += 1;
-    }
-    
-    generations++;
-
-    if (generations % GAMES_PER_GENERATION == 0) {
-        //console.log("Finish Generation of", training);
-        players[training].breed();
-    }
-    
-    setTimeout(newGeneration, 1);
-}
-
-function runDot(game, i) {
+function runDot(network, i) {
     var dot = dots[i];
     if (!dot.tagged) {
-        var network = players[i].networks[training == i ? game : 0];
 
         for (var j = 0; j < dots.length; j++) {
             network.setInputValue(j*2, dots[j].x);
@@ -119,7 +75,7 @@ function runDot(game, i) {
         if (dot.y < 0) dot.y = 0;
         if (dot.y > 1) dot.y = 1;
         
-        if (dot.x != lx || dot.y != ly) games[game].moves += 1;
+        if (dot.x != lx || dot.y != ly) moves += 1;
     
         if (dot.tagger) {
             for (var j = 0; j < dots.length; j++) {
@@ -128,34 +84,82 @@ function runDot(game, i) {
                         Math.abs(dot.x - d2.x) < 0.05 && 
                         Math.abs(dot.y - d2.y) < 0.05) {
                     d2.tagged = true;
-                    if (i == training)
-                        players[i].scores[game] += 1;
+                    dot.tagged_count += 1;
                 }
             }
         }
     }
 }
 
-function run() {
-    dt = 1/20;
-    ticks += dt;
-
-    if (ticks >= SECONDS_PER_GAME) {
-        finishGeneration();
-        return;
+function runGame(networks) {
+    ticks = 0;
+    dots = [];
+    for (var i = 0; i < networks.length; i++) {
+        var x = Math.random();
+        var y = Math.random();
+        dots.push({
+            x: x, 
+            y: y, 
+            tagged: false, 
+            tagged_count: 0,
+            tagger: i < TAGGER_COUNT
+        });
     }
-
-    for (var j = 0; j < GAMES; j++) {
-        if (games[j].toMove) {
-            games[j].moves = 0;
-            dots = games[j].dots;
-            for (var i = 0; i < dots.length; i++) {
-                runDot(j, i);
-            }
-            games[j].toMove = games[j].moves > 0;
+    
+    while ((ticks+=dt) < SECONDS_PER_GAME) {
+        moves = 0;
+        
+        for (var i = 0; i < dots.length; i++) {
+            runDot(networks[i], i);
+        }
+        
+        if (moves == 0 || dots[1].tagged) {
+            break; // Game over
         }
     }
-    run();
+}
+
+function runGeneration(training) {
+    players[training].evolve();
+    for (var j = 0; j < GAMES; j++) {
+        for (var i = 0; i < GAMES_PER_GENERATION*1.5; i++) {
+            var networks = [];
+            for (var k = 0; k < players.length; k++) {
+                if (i < GAMES_PER_GENERATION || training == k) { // Fight current agents
+                    networks.push(players[k].networks[training == k ? j : 0]);
+                } else { // Fight previous agents
+                    networks.push(previousBest[k]);
+                }
+            }
+            
+            runGame(networks);
+            
+            if (dots[training].tagger) // Is tagger
+                players[training].scores[j] += dots[training].tagged_count;
+            else if (!dots[training].tagged) // Is runner and hasn't been tagged
+                players[training].scores[j] += 1;
+        }
+    }
+    players[training].breed();
+    generations++;
+}
+
+function run() {
+    var t = Date.now();
+
+    runGeneration(training);
+
+    if (generations % (GENERATIONS_PER_TRAINING) == 0) {
+        previousBest[training] = players[training].networks[0];
+    
+        training = (training + 1) % PLAYER_COUNT;
+        console.log("Begin training of", training);
+        io.emit('msg', (training?'Red':'Blue') + ' dot has been trained');
+        writeBestToDisk();
+    }
+    
+    var dt = Date.now() - t;
+    setTimeout(run, 2*(1-CPU_TARGET/100)*dt);
 }
 
 function writeBestToDisk() {
@@ -184,7 +188,7 @@ fs.readFile("players.json", "utf-8", (err, data) => {
         }
     }
     
-    newGeneration();
+    run();
 })
 
 http.listen(1612, function(){
